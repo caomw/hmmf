556a3d8261717e2d498465843577a015d25a08ef
pratikac <pratikac@d6d04add-692a-0410-8435-c93ef0a91bfa>
2010-12-21 01:47:15 +0000

    Working FAST with opencv
    git-svn-id: svn+ssh://ares.lids.mit.edu/code/pratikac@1172 d6d04add-692a-0410-8435-c93ef0a91bfa

--- visinfo/sift.cpp
+++ visinfo/sift.cpp
@@ -1,24 +1,50 @@
-#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc_c.h"
+
 #include <iostream>
 #include <vector>
 
 using namespace cv;
 using namespace std;
 
+int threshold = 50;
 
-int process(VideoCapture& capture) 
+int process(CvCapture *capture) 
 {
 	string window_name = "video | q or esc to quit";
 	cout << "press q or esc to quit" << endl;
 	namedWindow(window_name, CV_WINDOW_KEEPRATIO); //resizable window;
-	Mat frame;
+
 	for (;;) 
     {
-		capture >> frame;
-		if (frame.empty())
-			continue;
-		imshow(window_name, frame);
-        cout<<frame.cols<<" "<<frame.rows<<endl;
+        IplImage *img = 0;
+
+		if (!cvGrabFrame(capture))
+        {
+            printf("Couldn't capture frame\n");
+			return 1;
+        }
+        img = cvRetrieveFrame(capture);
+        IplImage *newimg = cvCreateImage(cvSize(img->width, img->height), IPL_DEPTH_8U, 1);
+        cvConvertImage(img, newimg, CV_BGR2GRAY);
+        Mat frame(newimg);
+
+        vector<KeyPoint> keypoints;
+        FAST(frame, keypoints, threshold, true);
+        IplImage toshow = frame;
+
+        int count = 0;
+        for(int i=0; i< keypoints.size(); i++)
+        {
+            count++;
+            //cout<<i<<" "<<keypoints[i].pt.x<<" "<<keypoints[i].pt.y<<" "<<endl;
+            cvCircle(&toshow, cvPoint(keypoints[i].pt.x, keypoints[i].pt.y), 5, Scalar(0, 0, 255), 2);
+        }
+
+		imshow(window_name, &toshow);
+        cout<<count<<endl;
 		char key = (char)waitKey(5); //delay N millis, usually long enough to display and capture input
 		switch (key) 
         {
@@ -33,21 +59,14 @@ int process(VideoCapture& capture)
 	return 0;
 }
 
-int main(int ac, char** av) 
+int main(int argc, char** argv) 
 {
-	if (ac != 2) 
-	{
-	    cout<< "Pass video input\n";
-		return 1;
-	}
-	std::string arg = av[1];
-	VideoCapture capture(arg);
-	if (!capture.isOpened())
-		capture.open(atoi(arg.c_str()));
-	if (!capture.isOpened()) 
-	{
-		cerr << "Failed to open a video device or video file!\n" << endl;
-		return 1;
-	}
-	return process(capture);
+    if(argc != 2)
+        printf("Usage: ./sift <thresh>\n");
+    else
+    {
+        threshold = atoi(argv[1]);
+        CvCapture *capture = cvCaptureFromCAM(0);
+        return process(capture);
+    }
 }
