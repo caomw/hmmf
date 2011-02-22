481984c92690039f66439db632590639ae6545ed
pratikac <pratikac@d6d04add-692a-0410-8435-c93ef0a91bfa>
2011-02-22 18:47:18 +0000

    implicit filter still does not work
    git-svn-id: svn+ssh://ares.lids.mit.edu/code/pratikac@1260 d6d04add-692a-0410-8435-c93ef0a91bfa

--- pf/pf4.py
+++ pf/pf4.py
@@ -4,10 +4,11 @@ from numpy import *
 from numpy.random import *
 from pylab import *
 
-sigobs = 1.0
+dt = 0.01
+Q = 0.01
 
 def system(x):
-    return x
+    return -10*x*(x*x - 1)
 
 def obs(x):
     return x
@@ -45,28 +46,37 @@ def implicit_filter(y, N):
     while k < len(y):
         for i in range(N):
             zeta = normal(0, 1.0)
-            xj = 1.
-            xj1 = 0.0
+            xj = 0.5
+            xj1 = 0
             phij = 0.
-            sigj = 1.0
+            sigj = 0.0
             mbarj = 0.
+            Fn = system(xpart[i])*dt
+            Gn2 = dt
+            Q2 = Q*Q
+
             while(abs(xj1 - xj) > 0.001):
                 xj = xj1
                 
-                sigj = 1/(1. + obsx(xj)*obsx(xj))
-                zj = y[k] - obs(xj) + obsx(xj)
-                mbarj = sigj*(xpart[i] + system(xpart[i]) + obsx(xj)*zj)
-                kj = obsx(xj)*obsx(xj) + 1.
-                phij = pow( (zj - obsx(xj)*(xpart[i] + system(xpart[i]))), 2)/kj/2
+                sigj = 1/(1/Gn2 + obsx(xj)/Q2*obsx(xj))
+                zj = y[k] - obs(xj) + obsx(xj)*xj
+                mbarj = sigj*( 1/Gn2*(xpart[i] + Fn) + obsx(xj)*zj/Q2)
+                kj = Gn2*obsx(xj)*obsx(xj) + Q2
+                phij = pow( (zj - obsx(xj)*(xpart[i] + Fn)), 2)/kj/2
 
                 xj1 = mbarj + sqrt(sigj)*zeta
                 
+                #print y[k], xj1, sigj, mbarj
+                #raw_input()
+
             J = zeta*sigj/(xj1 - mbarj)
             
             xpart[i] = xj1
             w[i] = exp(-1*phij)*abs(J)
-
-        #xpart, w = resample(xpart, w)
+        
+        #print
+        w /= sum(w)
+        xpart, w = resample(xpart, w)
         """ 
         # updated all particles, now resample
         ct = std(w)**2/mean(w)**2
@@ -75,7 +85,6 @@ def implicit_filter(y, N):
             #resample
             xpart, w = resample(xpart, w)
         """
-        w /= sum(w)
         yhat[k] = sum(xpart*w)
         #print y[k], yhat[k]
         #temp = raw_input()
@@ -85,22 +94,30 @@ def implicit_filter(y, N):
 
 if __name__=="__main__":
     
-    t = 1000
-    dt = 0.001
+    t = 100
     x = zeros(t, float)
     y = zeros(t, float)
      
-    x[0] = 0.
+    x[0] = 1.0
     y[0] = x[0]
     i = 1
     while i< t:
-        x[i] = x[i-1] + system(x[i-1])*dt + sqrt(dt)*normal(0, 1.)
-        y[i] = obs(x[i]) + normal(0, sigobs)
+        x[i] = x[i-1] + system(x[i-1])*dt + normal(0, dt)
+        y[i] = obs(x[i]) + Q*normal(0, 1.0)
         i += 1
-        
-    yhat = implicit_filter(y, 50)    
-    
+   
+    """
+    fig1 = figure(1)
     plot(x, 'r-')
-    plot(yhat, 'g-')
+    grid()
+    fig1.show()
+    """
+
+    yhat = implicit_filter(y, 100)    
+    
+    figure(2)
+    plot(x, 'r-')
+    plot(y, 'b-')
+    plot(yhat, 'g-', lw=0.5)
     grid()
     show()
