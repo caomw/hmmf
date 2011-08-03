bcaa4ad18153477a1f235c202e71e4e370c3b72e
pratikac <pratikac@d6d04add-692a-0410-8435-c93ef0a91bfa>
2011-08-03 16:58:16 +0000

    Added dubins car integration
    git-svn-id: svn+ssh://ares.lids.mit.edu/code/pratikac@1630 d6d04add-692a-0410-8435-c93ef0a91bfa

---
+++ pf/hmm/systems/dubins.cpp
@@ -0,0 +1,166 @@
+#include "dubins.h"
+
+System::System()
+{
+    min_states = new double[NUM_DIM];
+    min_states[0] = 0; min_states[1] = 0; min_states[2] = 0; min_states[3] = 0;
+    
+    max_states = new double[NUM_DIM];
+    max_states[0] = 1; max_states[1] = 1; max_states[2] = 1; max_states[3] = M_PI_2;
+
+    obs_noise = new double[NUM_DIM-1];
+    obs_noise[0] = 1e-4; obs_noise[1] = 1e-4; obs_noise[2] = 1e-4;
+    
+    process_noise = new double[NUM_DIM-1];
+    process_noise[0] = 1e-4; process_noise[1] = 1e-4; process_noise[2] = 1e-4;
+    
+    init_var = new double[NUM_DIM-1];
+    init_var[0] = 1e-4; init_var[1] = 1e-4; init_var[2] = 1e-4;
+    
+    init_state.x[0] = 0;
+    init_state.x[1] = 0;
+    init_state.x[2] = 0;
+    init_state.x[3] = 0;
+
+    sim_time_delta = 0.01;
+}
+
+System::~System()
+{
+    delete[] min_states;
+    delete[] max_states;
+    delete[] obs_noise;
+    delete[] process_noise;
+    delete[] init_var;
+}
+
+State System::sample()
+{
+    State s;
+    while(1)
+    {
+        for(int i=0; i< NUM_DIM; i++)
+        {
+            s.x[i] = min_states[i] + RANDF*( max_states[i] - min_states[i]);
+        }
+
+        if( is_free(s) )
+            break;
+    }
+    return s;
+}
+
+bool System::is_free(State &s)
+{
+    return 1;
+
+    bool retflag = 0;
+
+    // obs 1
+    if( (s[0] >= 0.127) && (s[0] <= 0.26) )
+    {
+        if( (s[1] >= 0) && (s[1] <= .217) )
+            retflag = 0;
+        else
+            retflag = 1;
+    }
+    else
+        retflag = 1;
+
+    if (retflag == 0)
+        return 0;
+
+    // obs 2
+    if( (s[0] >= 0.1) && (s[0] <= 0.2) )
+    {
+        if( (s[1] >= .32) && (s[1] <= .5) )
+            retflag = 0;
+        else
+            retflag = 1;
+    }
+    else
+        retflag = 1;
+
+    return retflag;
+}
+
+int System::get_key(State& s, double *key)
+{
+    for(int i =0; i < NUM_DIM; i++)
+    {
+        key[i] = (s.x[i] - min_states[i])/(max_states[i] - min_states[i]);
+        assert(key[i] <= 1.1);
+    }
+    return 0;
+}
+
+State System::integrate(State& s, double duration, bool is_clean)
+{
+    State t;
+
+    double *var = new double[NUM_DIM-1];
+    double *mean = new double[NUM_DIM-1];
+    double *tmp = new double[NUM_DIM-1];
+
+    for(int i=0; i<NUM_DIM; i++)
+    {
+        t.x[i] = s.x[i];
+    }
+
+    for(int i=0; i<NUM_DIM-1; i++)
+    {
+        var[i] = process_noise[i]*duration;
+        tmp[i] = 0;
+        mean[i] = 0;
+    }
+    if( !is_clean)  
+        multivar_normal( mean, var, tmp, NUM_DIM-1);
+        
+    double curr_time = 0;
+    while(curr_time < duration)
+    {
+        t.x[0] += sim_time_delta;
+        t.x[1] += 1.0*cos(t.x[3])*sim_time_delta;
+        t.x[2] += 1.0*sin(t.x[3])*sim_time_delta;
+        t.x[3] += 1.0*sim_time_delta;
+
+        curr_time += sim_time_delta;
+    }
+
+    for(int i=1; i<NUM_DIM; i++)
+        t.x[i] += tmp[i-1];
+
+    delete[] mean;
+    delete[] tmp;
+    delete[] var;
+
+    return t;
+}
+
+
+State System::observation(State& s, bool is_clean)
+{
+    State t;
+
+    double *tmp = new double[NUM_DIM-1];
+    double *mean = new double[NUM_DIM-1];
+
+    if( !is_clean)  
+        multivar_normal( mean, obs_noise, tmp, NUM_DIM-1);
+
+    if(is_clean)
+    {
+        for(int i=0; i<NUM_DIM-1; i++)
+            tmp[i] = 0;
+    }
+
+    t.x[0] = s.x[0];                        // time is same
+    for(int i=1; i<NUM_DIM; i++)
+        t.x[i] = s.x[i] + tmp[i-1];
+
+    delete[] mean;
+    delete[] tmp;
+
+    return t;
+}
+
---
+++ pf/hmm/systems/dubins.h
@@ -0,0 +1,79 @@
+#ifndef __dubins_h__
+#define __dubins_h__
+
+#include "../common.h"
+
+#define NUM_DIM     (4)
+
+class State
+{
+    public:
+        double x[NUM_DIM];
+
+        State()
+        {
+        }
+        State(double *val)
+        {
+            for(int i=0; i<NUM_DIM; i++)
+                x[i] = val[i];
+        }
+        ~State()
+        {
+        }
+
+        double operator[](int which_dim)
+        {
+            assert(which_dim < NUM_DIM);
+            return x[which_dim];
+        }
+        State& operator=(const State &that)
+        {
+            if(this != &that)
+            {
+                for(int i=0; i< NUM_DIM; i++)
+                    x[i] = that.x[i];
+                
+                return *this;
+            }
+            else
+                return *this;
+        }
+};
+
+class System
+{
+    public:
+
+        double *obs_noise;
+        double *process_noise;
+        double *init_var;
+
+        double *min_states;
+        double *max_states;
+        double sim_time_delta;
+
+        State init_state;
+
+        System();
+        ~System();
+
+        // functions
+        
+        int get_key(State& s, double *key);
+        bool is_free(State &s);
+        State sample();
+        State integrate(State& s, double duration, bool is_clean);
+        State observation(State& s, bool is_clean);
+};
+
+inline double dist(State& s1, State& s2)
+{
+    double t = 0;
+    for(int i=0; i<NUM_DIM; i++)
+        t += (s1.x[i] - s2.x[i])*(s1.x[i] - s2.x[i]);
+    
+    //cout<<"dist: "<< t << endl;
+    return sqrt(t);
+};
+#endif
